4b7850f1ce74d89be7c992289908261cf9089b43
qiuzhichang <qiu_zhi_chang@126.com>
2020-06-06 15:13:34 +0800

    close profiler

--- ORB2-mac/Examples/Monocular/mono_euroc.cc
+++ ORB2-mac/Examples/Monocular/mono_euroc.cc
@@ -47,14 +47,14 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    ProfilerStart("ORB_SLAM2_TOTAL.prof");
+//    ProfilerStart("ORB_SLAM2_TOTAL.prof");
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
     auto resultFuture = async(launch::async, processing, argv, &SLAM);
     SLAM.RunViewer();
 
-    ProfilerStop();
+//    ProfilerStop();
 
     return resultFuture.get();
 }
